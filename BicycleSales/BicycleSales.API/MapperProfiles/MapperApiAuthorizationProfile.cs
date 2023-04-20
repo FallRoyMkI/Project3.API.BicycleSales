30e98d3723b801b95f2b5341bd0fc18ec7ef10f5
Svyatoslav <svat.strel.2001@gmail.com>
2023-04-20 03:11:20 +0300

    add map acceptance profile

--- BicycleSales/BicycleSales.API/MapperProfiles/MapperApiAuthorizationProfile.cs
+++ BicycleSales/BicycleSales.API/MapperProfiles/MapperApiAuthorizationProfile.cs
@@ -2,6 +2,8 @@
 using BicycleSales.API.Models.AuthorizationProduct.Request;
 using BicycleSales.BLL.Models;
 using AutoMapper;
+using BicycleSales.API.Models.Acceptance.Request;
+using BicycleSales.API.Models.Acceptance.Response;
 
 namespace BicycleSales.API.MapperProfiles;
 
@@ -16,5 +18,8 @@ public class MapperApiAuthorizationProfile : Profile
 
         CreateMap<AuthorizationUpdateRequest, Authorization>();
         CreateMap<AuthorizationUpdateByAdminRequest, Authorization>();
+
+        CreateMap<AcceptanceAddRequest, Acceptance>().ForMember(t => t.FormedBy, x => x.MapFrom(y => y.UserIdWhichFormed));
+        CreateMap<Acceptance, AcceptanceResponse>();
     }
 }
